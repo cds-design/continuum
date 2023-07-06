fcb06352b3b8dfbde9a424a366ee977adf6c1c9e
Rajaniraiyn R <72294760+Rajaniraiyn@users.noreply.github.com>
2023-07-06 14:17:24 +0530

    ignore autogenerated

--- types/src/WC.d.ts
+++
@@ -1,8 +0,0 @@
-import { LitElement, PropertyValueMap } from "lit";
-export declare class WC extends LitElement {
-  className: string;
-  connectedCallback(): void;
-  protected updated(
-    _changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>,
-  ): void;
-}
--- types/src/constants.d.ts
+++
@@ -1,3 +0,0 @@
-export { version as VERSION } from "../package.json";
-export declare const TAG_NAME_PREFIX = "cds";
-export { default as GLOBAL_CSS } from "./styles/globals.css";
